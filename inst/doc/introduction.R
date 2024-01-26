d6503db665bfeaa9be38320a952aa70c0d855dc7
Marcus Beck <mbeck@tbep.org>
2024-01-26 15:30:02 +0000

    version 1.4.2

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   comment = "#>",
   message = F, warning = F, 
@@ -25,14 +25,14 @@ mod
 anlz_smooth(mod)
 anlz_fit(mod)
 
-## ---- fig.height = 5, fig.width = 6-------------------------------------------
+## ----fig.height = 5, fig.width = 6--------------------------------------------
 ylab <- "Chlorophyll-a"
 show_prddoy(mod, ylab = ylab)
 
-## ---- fig.height = 4, fig.width = 6-------------------------------------------
+## ----fig.height = 4, fig.width = 6--------------------------------------------
 show_prdseries(mod, ylab = ylab)
 
-## ---- fig.height = 4, fig.width = 6-------------------------------------------
+## ----fig.height = 4, fig.width = 6--------------------------------------------
 show_prdseason(mod, ylab = ylab)
 
 ## -----------------------------------------------------------------------------
@@ -41,7 +41,7 @@ show_prd3d(mod, ylab = ylab)
 ## -----------------------------------------------------------------------------
 anlz_perchg(mod, baseyr = 2006, testyr = 2017)
 
-## ---- fig.height = 4, fig.width = 9-------------------------------------------
+## ----fig.height = 4, fig.width = 9--------------------------------------------
 show_perchg(mod, baseyr = 2006, testyr = 2017, ylab = "Chlorophyll-a (ug/L)")
 
 ## -----------------------------------------------------------------------------
@@ -51,25 +51,34 @@ metseason
 ## -----------------------------------------------------------------------------
 anlz_mixmeta(metseason, yrstr = 2006, yrend = 2017)
 
-## ---- fig.height = 4, fig.width = 9, echo = T---------------------------------
+## ----fig.height = 4, fig.width = 9, echo = T----------------------------------
 show_metseason(mod, doystr = 90, doyend = 180, yrstr = 2006, yrend = 2017, ylab = "Chlorophyll-a (ug/L)")
 
-## ---- fig.height = 4, fig.width = 9, echo = T---------------------------------
+## ----fig.height = 4, fig.width = 9, echo = T----------------------------------
 show_metseason(mod, doystr = 90, doyend = 180, yrstr = NULL, yrend = NULL, ylab = "Chlorophyll-a (ug/L)")
 
-## ---- fig.height = 4, fig.width = 9, echo = T---------------------------------
+## ----fig.height = 4, fig.width = 9, echo = T----------------------------------
 show_metseason(mod, metfun = max, nsim = 100, doystr = 90, doyend = 180, yrstr = 2006, yrend = 2017, ylab = "Chlorophyll-a (ug/L)")
 
 ## -----------------------------------------------------------------------------
 trndseason <- anlz_trndseason(mod, doystr = 90, doyend = 180, justify = 'left', win = 5)
 head(trndseason)
 
-## ---- fig.height = 5, fig.width = 9, echo = T---------------------------------
+## ----fig.height = 5, fig.width = 9, echo = T----------------------------------
 show_trndseason(mod, doystr = 90, doyend = 180, justify = 'left', win = 5, ylab = 'Chl. change/yr, average')
 
-## ---- fig.height = 5, fig.width = 9, echo = T---------------------------------
+## ----fig.height = 5, fig.width = 9, echo = T----------------------------------
 show_trndseason(mod, metfun = max, nsim = 100, doystr = 90, doyend = 180, justify = 'left', win = 5, ylab = 'Chl. change/yr, maximum')
 
-## ---- fig.height = 7, fig.width = 9, echo = T---------------------------------
+## ----fig.height = 5, fig.width = 9, echo = T----------------------------------
+show_trndseason(mod, metfun = max, nsim = 100, doystr = 90, doyend = 180, justify = 'left', win = 5, ylab = 'Chl. change/yr, maximum', usearrow = T)
+
+## ----fig.height = 7, fig.width = 9, echo = T----------------------------------
 show_sumtrndseason(mod, doystr = 90, doyend = 180, justify = 'left', win = 5:15)
+
+## ----fig.height = 5, fig.width = 9, echo = T----------------------------------
+show_mettrndseason(mod, metfun = mean, doystr = 90, doyend = 180, ylab = "Chlorophyll-a (ug/L)", win = 5, justify = 'left')
+
+## ----fig.height = 5, fig.width = 9, echo = T----------------------------------
+show_mettrndseason(mod, metfun = mean, doystr = 90, doyend = 180, ylab = "Chlorophyll-a (ug/L)", win = 5, justify = 'left', cmbn = T)
 
---
+++ tests/testthat/test-show_mettrndseason.R
@@ -0,0 +1,27 @@
+test_that("Checking show_mettrndseason class", {
+  
+  result <- show_mettrndseason(mod, doystr = 90, doyend = 180, justify = 'left', win = 5, ylab = 'Chlorophyll-a (ug/L)', nsim = 10)
+  
+  expect_is(result, 'ggplot')
+  
+})
+
+test_that("Checking show_mettrndseason class with cmbn as T", {
+  
+  result <- show_mettrndseason(mod, doystr = 90, doyend = 180, justify = 'left', win = 5, ylab = 'Chlorophyll-a (ug/L)', cmbn = T, nsim = 10)
+  
+  expect_is(result, 'ggplot')
+  
+})
+
+test_that("show_mettrndseason throws an error if cols length is not 4 for cmbn as F", {
+
+  expect_error(show_mettrndseason(mod, cols = c('red', 'blue'), nsim = 10), "Four names or colors must be provided")
+  
+})
+
+test_that("show_mettrndseason throws an error if cols length is not 3 for cmbn as T", {
+  
+  expect_error(show_mettrndseason(mod, cols = c('red', 'blue'), cmbn = T, nsim = 10), "Three names or colors must be provided")
+  
+})
